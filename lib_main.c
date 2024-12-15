264675d342fd19be59ad79db80851669bfd48783
jilli-n <164900305+jilli-n@users.noreply.github.com>
2024-12-15 11:52:27 -0500

    fully functioning checkout, need to fix and implement return

--- lib_main.c
+++ lib_main.c
@@ -70,7 +70,7 @@ void LoadBooks(FILE *f, Node **root) {
         book.category = malloc(50);
         book.status = malloc(50);
 
-        if (sscanf(line, "%d,%49[^,],%49[^,],%49[^,],%49[^,]", &book.id, book.title, book.author, book.category, book.status) == 5) {
+        if (sscanf(line, "%d,%49[^,],%49[^,],%49[^,],%49[^]", &book.id, book.title, book.author, book.category, book.status) == 5) {
             insert_end(root, book);
         } else {
             free(book.title);
@@ -89,6 +89,80 @@ void to_lowercase(char *dest, const char *src) {
 }
 
 void checkOut(Node *selectedBook);
+
+int changeAvailability(Node *selectedBook){
+    char filename[50] = "Books_data - Sheet1.csv";
+    char temp_filename[50] = "temp_Books_data - Sheet1.csv";
+    char buffer[400], replace[400];
+    int replace_line = selectedBook->book.id;
+    FILE *file = fopen(filename,"r");
+    FILE *temp = fopen(temp_filename,"w");
+
+    if (selectedBook->book.status[0]=='a'){
+       strcpy(selectedBook->book.status,"issued");
+    } else{
+       strcpy(selectedBook->book.status,"available");
+    }
+
+    if (file == NULL || temp == NULL)
+    {
+        printf("File Error\n");
+        return 1;
+    }
+
+    bool keep_reading = true;
+    int current_line = 1;
+  
+    do{
+        fgets(buffer,400,file);
+        if (feof(file)) keep_reading = false;
+        else if (current_line == replace_line)
+            fprintf(temp, "%d,%s,%s,%s,%s\n", selectedBook->book.id,selectedBook->book.title,selectedBook->book.author,selectedBook->book.category,selectedBook->book.status);
+        else{
+            Book tempbook;
+            tempbook.title = malloc(50);
+            tempbook.author = malloc(50);
+            tempbook.category = malloc(50);
+            tempbook.status = malloc(50);
+            sscanf(buffer, "%d,%49[^,],%49[^,],%49[^,],%49[^]", &tempbook.id, tempbook.title, tempbook.author, tempbook.category, tempbook.status);
+            fprintf(temp, "%d,%s,%s,%s,%s", tempbook.id, tempbook.title, tempbook.author, tempbook.category, tempbook.status);
+        }
+        current_line++;
+    }while(keep_reading);
+    fprintf(temp, "");
+    fclose(file);
+    fclose(temp);
+    remove(filename);
+    rename(temp_filename, filename);
+
+    return 0;
+}
+
+void ReturnBook(Node *root){
+    char bookreturn[100];
+    char bookReturnLower[100];
+    char action;
+    Node *current = root;
+    int found = 0;
+    printf("Please enter the name of the book you're returning: ");
+    scanf(" %s", bookreturn);
+    to_lowercase(bookReturnLower,bookreturn);
+    while (current != NULL) {
+        char field[100] = "";
+        to_lowercase(field,current->book.title);
+        if (strstr(field,bookReturnLower)) {
+            printf("Book Found: \n");
+            printf("%d. %s by %s ,Genre: %s, Status: %s\n", current->book.id, current->book.title, current->book.author, current->book.category, current->book.status);
+            printf("Returning...\n");
+            found = 1;
+            changeAvailability(current);
+            break;
+        }
+        current = current->next;
+    }
+    if (!found) printf("No books found matching the search criteria.\n");
+
+}
 
 void Search(Node *root)
 {
@@ -208,6 +282,7 @@ void checkOut(Node *selectedBook)
 {
     if (selectedBook->book.status[0]=='a'){
         printf("The book is available. Checking it out...\n");
+        changeAvailability(selectedBook);
     } else{
         printf("Book not available\n");
     }
